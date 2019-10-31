3e0ab951c1e2121b1715e02efdcfb7e8e38733d6
Felipe <felipeiasi@gmail.com>
2019-10-31 20:12:04 -0300

    Templates

--- app/video/urls.py
+++ app/video/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'video'
 
 urlpatterns = [
-    path('', views.lista_canais, name='lista_canais'),
+    path('', views.list_channels, name='list_channels'),
     path('channels/', views.channels, name='channels'),
-    path('<slug:channel>/', views.video, name='video'),
-    path('<slug:channel>/<int:day>/<int:month>/<int:year>/<int:hour>/<int:minute>/', views.video, name='video'),
+    path('channel/<int:pk>/edit/', views.channels, name='channel_edit'),
+    # path('<slug:channel>/', views.video, name='video'),
+    # path('<slug:channel>/<int:day>/<int:month>/<int:year>/<int:hour>/<int:minute>/', views.video, name='video'),
 ]
--- app/video/views.py
+++ app/video/views.py
@@ -1,14 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Channel, Clip
 from datetime import datetime
+from .forms import ChannelForm
 
 
-def lista_canais(request):
+def list_channels(request):
     channels = Channel.objects.all()
+
     return render(request, 'video/lista_canais.html', {'title': 'Lista de Canais', 'channels': channels})
+
+
+def channels(request):
+    channels = Channel.objects.all()
+
+    new_channel = None
+
+    if request.method == 'POST':
+        # Um novo post será criado
+        channel_form = ChannelForm(data=request.POST)
+        if channel_form.is_valid():
+            new_channel = channel_form.save()
+    else:
+        channel_form = ChannelForm()
+
+    return render(request, 'video/channels.html', {'channels': channels,
+                                                   'channel_form': channel_form,
+                                                   'new_channel': new_channel})
+
+
+def channel_edit(request, pk):
+    channel = get_object_or_404(Channel, pk=pk)
+    if request.method == "POST":
+        channel_form = ChannelForm(request.POST, instance=channel)
+        if channel_form.is_valid():
+            channel = channel_form.save()
+            return render('post_detail', pk=channel.pk)
+    else:
+        form = ChannelForm(instance=channel)
+    return render(request, 'blog/channel_edit.html', {'channel_form': channel_form})
 
 
 def video(request, channel=None, date=None, time=None):
@@ -25,10 +57,5 @@ def video(request, channel=None, date=None, time=None):
             clip = Clip.objects.filter(channel__slug=channel, recordDate__lte=diaHora).order_by('-recordDate').first()
         else:
             clip = Clip.objects.filter(channel__slug=channel).order_by('-recordDate').first()
-    print(request)
+
     return render(request, 'video/video.html', {'title': 'Player', 'clip': clip, 'date': date, 'time': time})
-
-
-def channels(request):
-    channels = Channel.objects.all()
-    return render(request, 'video/channels.html', {'title': "Administrar Canais", 'channels': channels})
